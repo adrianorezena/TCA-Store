1516d5adf327635d25f6fcb5637107564f6b601b
adrianorezena <adrianocr@gmail.com>
2023-04-26 08:19:00 -0300

    add `ProductCell`

---
+++ TCA Store Reducer Protocol/Scenes/Home/ProductCell/ProductCell.swift
@@ -0,0 +1,68 @@
+//
+//  ProductCell.swift
+//  TCA Store Reducer Protocol
+//
+//  Created by Adriano Rezena on 26/04/23.
+//
+
+import TCANetwork
+import ComposableArchitecture
+import SwiftUI
+
+struct ProductCell: View {
+    let store: StoreOf<ProductCellReducer>
+    
+    var body: some View {
+        WithViewStore(store) { viewStore in
+            VStack(spacing: 16) {
+                AsyncImage(url: URL(string: viewStore.product.image)) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 200)
+                } placeholder: {
+                    ProgressView()
+                        .frame(height: 200)
+                }
+                
+                Text(viewStore.product.title)
+                    .fontWeight(.bold)
+                
+                HStack {
+                    Text("$\(viewStore.product.price.description)")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+//                    AddToCartButton
+                }
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(
+                RoundedRectangle(cornerRadius: 8)
+                    .fill(.white)
+            )
+        }
+    }
+}
+
+struct ProductCell_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductCell(
+            store: Store(
+                initialState: ProductCellReducer.State(
+                    product: Product(
+                        id: 1,
+                        title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+                        price: 109.95,
+                        description: "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+                        category: "men's clothing",
+                        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                    )
+                ),
+                reducer: ProductCellReducer())
+        )
+    }
+}
---
+++ TCA Store Reducer Protocol/Scenes/Home/ProductCell/ProductCellReducer.swift
@@ -0,0 +1,27 @@
+//
+//  ProductCellReducer.swift
+//  TCA Store Reducer Protocol
+//
+//  Created by Adriano Rezena on 26/04/23.
+//
+
+import TCANetwork
+import ComposableArchitecture
+import Foundation
+
+struct ProductCellReducer: ReducerProtocol {
+    struct State: Equatable {
+        var product: Product
+    }
+    
+    enum Action: Equatable, BindableAction {
+        case binding(BindingAction<State>)
+    }
+    
+    func reduce(into state: inout State, action: Action) -> EffectTask<Action> {
+        switch action {
+        case .binding:
+            return .none
+        }
+    }
+}
